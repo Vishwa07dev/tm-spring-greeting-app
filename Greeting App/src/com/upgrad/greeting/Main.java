94d9bc8c9f03d098689095cf4982774827878e4b
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-15 23:10:20 +0530

    hands-on for spring framework component annotation

--- Greeting App/src/com/upgrad/greeting/Main.java
+++ Greeting App/src/com/upgrad/greeting/Main.java
@@ -1,6 +1,5 @@
 package com.upgrad.greeting;
 
-import com.upgrad.greeting.factories.TimeServiceFactory;
 import com.upgrad.greeting.services.GreetingService;
 import com.upgrad.greeting.services.TimeService;
 import org.springframework.context.ApplicationContext;
@@ -15,8 +14,7 @@ public class Main {
 
         greetingService.greet("John");
 
-        TimeServiceFactory timeServiceFactory = new TimeServiceFactory();
-        TimeService timeService = timeServiceFactory.getTimeService("24hour");
+        TimeService timeService = (TimeService)context.getBean("timeService24HourFormat");
         System.out.println(timeService.getCurrentTime());
     }
 }
--- Greeting App/src/com/upgrad/greeting/services/TimeService24HourFormat.java
+++ Greeting App/src/com/upgrad/greeting/services/TimeService24HourFormat.java
@@ -1,7 +1,10 @@
 package com.upgrad.greeting.services;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 
+@Component
 public class TimeService24HourFormat implements TimeService {
     @Override
     public int getCurrentTime() {
