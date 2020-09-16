535d3eff4226022ea9ffd4c3e6966946b357175d
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-16 13:37:41 +0530

    hands-on for @Autowired annotation

--- Greeting App/src/com/upgrad/greeting/Main.java
+++ Greeting App/src/com/upgrad/greeting/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.upgrad.greeting");
-        GreetingService greetingService = (GreetingService)context.getBean("englishGreetingService");
+        GreetingService greetingService = (GreetingService)context.getBean("frenchGreetingService");
 
         greetingService.greet("John");
     }
--- Greeting App/src/com/upgrad/greeting/services/FrenchGreetingService.java
+++ Greeting App/src/com/upgrad/greeting/services/FrenchGreetingService.java
@@ -1,11 +1,23 @@
 package com.upgrad.greeting.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FrenchGreetingService implements GreetingService {
+
+    @Autowired
+    private TimeService timeService;
+
     @Override
     public void greet(String name) {
-        System.out.println("Bonjour, " + name);
+        int time = timeService.getCurrentTime();
+        String greetingMessage;
+        if (time < 17) {
+            greetingMessage = "Bonjour";
+        } else {
+            greetingMessage = "Bonsoir";
+        }
+        System.out.println(greetingMessage + ", " + name);
     }
 }
