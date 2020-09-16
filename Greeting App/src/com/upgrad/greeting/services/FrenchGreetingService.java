de2111b33d9f38db8f71170bbbf19f5bd2cd8158
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-16 14:13:12 +0530

    hands-on for Qualifier annotation

--- Greeting App/src/com/upgrad/greeting/services/FrenchGreetingService.java
+++ Greeting App/src/com/upgrad/greeting/services/FrenchGreetingService.java
@@ -1,12 +1,14 @@
 package com.upgrad.greeting.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FrenchGreetingService implements GreetingService {
 
     @Autowired
+    @Qualifier("timeService24HourFormat")
     private TimeService timeService;
 
     @Override
