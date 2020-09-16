8205b6ef9931b27b568c7953bb7cb4fd15b47253
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-16 15:20:37 +0530

    singleton scope demo

--- Greeting App/src/com/upgrad/greeting/Main.java
+++ Greeting App/src/com/upgrad/greeting/Main.java
@@ -9,8 +9,10 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.upgrad.greeting");
-        GreetingService greetingService = (GreetingService)context.getBean("frenchGreetingService");
+        GreetingService greetingService1 = (GreetingService)context.getBean("englishGreetingService");
+        GreetingService greetingService2 = (GreetingService)context.getBean("englishGreetingService");
 
-        greetingService.greet("John");
+        System.out.println(greetingService1 == greetingService2);
+
     }
 }
