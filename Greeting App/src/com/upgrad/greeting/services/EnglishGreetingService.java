dac9c3efbd6137375317ad2bdf0e2d194e43179a
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-16 14:09:04 +0530

    demo of qualifier annotation

--- Greeting App/src/com/upgrad/greeting/services/EnglishGreetingService.java
+++ Greeting App/src/com/upgrad/greeting/services/EnglishGreetingService.java
@@ -1,6 +1,7 @@
 package com.upgrad.greeting.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,7 @@ public class EnglishGreetingService implements GreetingService {
     private TimeService timeService;
 
     @Autowired
-    public EnglishGreetingService(TimeService timeService) {
+    public EnglishGreetingService(@Qualifier("timeService24HourFormat") TimeService timeService) {
         this.timeService = timeService;
     }
 
